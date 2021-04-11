661c4c57b526ce1b8f5f6d5ab9d845362afe2100
yuzhanglong <yuzl1123@163.com>
2021-04-11 14:22:40 +0800

    chore: add useful type declaration

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,4 +1,5 @@
 import { SET_LANGUAGE } from './constants'
+import { AnyAction } from 'redux'
 
 export interface AppStore {
   language: 'zh-CN' | 'en-US'
@@ -8,7 +9,7 @@ const initialState: AppStore = {
   language: 'zh-CN'
 }
 
-function reducer(state = initialState, action: any) {
+function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
     case SET_LANGUAGE:
       return { ...state, language: action.language }
